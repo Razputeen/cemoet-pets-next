63f3dc8fad75a3189efba0e3bf585cd0538e83b7
Senow <senasenaajiaji@gmail.com>
2025-07-22 15:21:15 +0700

    selasa-ncen

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,40 +1,30 @@
-"use client";
-import { useEffect, useState } from "react";
+import { AppSidebar } from "#/components/app-sidebar"
+import { ChartAreaInteractive } from "#/components/chart-area-interactive"
+import { DataTable } from "#/components/data-table"
+import { SectionCards } from "#/components/section-cards"
+import { SiteHeader } from "#/components/site-header"
+import { SidebarInset, SidebarProvider } from "#/components/ui/sidebar"
 
-type User = {
-  id: number;
-  Name: string;
-  email: string;
-  roles: string[];
-};
+import data from "./data.json"
 
-export default function DashboardPage() {
-  const [user, setUser] = useState<User | null>(null);
-
-    useEffect(() => {
-    const fetchUser = async () => {
-        const token = localStorage.getItem("token");
-
-        const res = await fetch("http://localhost:3222/auth/profile", {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            "cache-control": "no-cache",
-        },
-        cache: "no-store",
-        });
-
-        const data = await res.json();
-        setUser(data); // <- ini misalnya lo pake useState untuk nampung data user
-    };
-
-    fetchUser();
-    }, []);
-
-
+export default function Page() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    <p className="text-lg font-bold">Welcome, {user?.Name}</p>
-    </div>
-  );
+    <SidebarProvider>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
